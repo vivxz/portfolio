897b52d78c770324de48a27130ffaa6039e8e431
Vivian Quan <viviannquan@gmail.com>
2020-03-28 08:00:57 -0700

    [prog]: Added and styled about component to have an image and description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      ALL ABOUT ME!!!
+    <div className="about-info">
+      <img className="profile-picture" src="https://vivs-portfolio.s3-us-west-1.amazonaws.com/pp.png" alt="avatar" />
+      <p>
+        A meticulous, creative, and passionate
+        software engineer with a strong interest in
+        projects that require both conceptual and
+        analytical thinking. Fully-committed to
+        designing and developing innovative
+        applications that users will love. Always
+        eager to learn more from anyone or
+        anything.
+        </p>
+      <p>
+        I would love to join a team/company that
+        will allow me to leverage my skills,
+        encourage my passions, and continue to
+        challenge me as I grow in this field or in life.
+      </p>
     </div>
   )
 }
