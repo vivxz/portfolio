611b4c9427cdfe865a12ad9f8a0a684fcd39800b
Vivian Quan <viviannquan@gmail.com>
2020-03-29 19:46:18 -0700

    [prog]: Fixed responsivenessto resizing of about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,22 +4,24 @@ const About = () => {
   return (
     <div className="about-info">
       <img className="profile-picture" src="https://vivs-portfolio.s3-us-west-1.amazonaws.com/pp.png" alt="avatar" />
-      <p>
-        A meticulous, creative, and passionate
-        software engineer with a strong interest in
-        projects that require both conceptual and
-        analytical thinking. Fully-committed to
-        designing and developing innovative
-        applications that users will love. Always
-        eager to learn more from anyone or
-        anything.
+      <div className="about-intro">
+        <p>
+          A meticulous, creative, and passionate
+          software engineer with a strong interest in
+          projects that require both conceptual and
+          analytical thinking. Fully-committed to
+          designing and developing innovative
+          applications that users will love. Always
+          eager to learn more from anyone or
+          anything.
         </p>
-      <p>
-        I would love to join a team/company that
-        will allow me to leverage my skills,
-        encourage my passions, and continue to
-        challenge me as I grow in this field or in life.
+        <p>
+          I would love to join a team/company that
+          will allow me to leverage my skills,
+          encourage my passions, and continue to
+          challenge me as I grow in this field or in life.
       </p>
+      </div>
     </div>
   )
 }
