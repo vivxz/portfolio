e9e656c74d6ca8b1fe5483949d9374438f8938d4
Vivian Quan <viviannquan@gmail.com>
2020-03-31 20:03:55 -0700

    [prog]: Refactored typing effects from CSS to React Hooks

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar.jsx';
 
 const Home = () => {
+  const [fullName] = useState("Vivian Quan");
+  const [nameLength, setNameLength] = useState(0);
+
+  const addingLetters = () => {
+    setNameLength(nameLength + 1)
+  }
+
+  if (nameLength < fullName.length) {
+    setTimeout(addingLetters, 120);
+  }
+
   return (
     <div>
       <div className="content">
-        <h1 className="full-name">Vivian Quan</h1>
+        <h1 className="full-name">{fullName.slice(0, nameLength)}</h1>
+        {/* <span className="typewriter"></span> */}
         <p className="titles">Software Engineer | Developer | Creator</p>
       </div>
       <Navbar />
