c1d66239902f07d996f064da2b51602c2be5a3c8
Vivian Quan <viviannquan@gmail.com>
2020-04-03 22:36:36 -0700

    [prog]:Updated Contact component by adding in a form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,56 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      CONTACT ME!!!
+    <div className="contact-container" id="contact">
+      <div className="contact-card">
+        <div className="contact-form">
+          <form className="form" action="https://formspree.io/mlewdvkk" method="POST">
+            <h1>Contact me</h1>
+            <div className="form-item">
+              <label>
+                Name:
+                <input type="text" name="name" className="name" required />
+              </label>
+            </div>
+            <div className="form-item">
+              <label>
+                Email:
+              <input type="text" name="_replyto" id="email" required />
+              </label>
+            </div>
+            <div className="form-item">
+              <label>
+                Subject:
+              <input type="text" name="subject" id="subject" required />
+              </label>
+            </div>
+            <div className="form-item">
+              <label>
+                Message:
+              <textarea name="message" id="message" required />
+              </label>
+            </div>
+            <input type="submit" value="SEND" />
+          </form>
+        </div>
+        <div className="contact-info">
+          <h1>Let's get in touch</h1>
+          <div className="form-item">
+            <p>Email</p>
+            <p>viviannquan@gmail.com</p>
+          </div>
+          <div className="form-item">
+            <p>LinkedIn</p>
+            <a href="https://www.linkedin.com/in/vivian-quan/" target="_blank">vivian-quan</a>
+          </div>
+          <div className="form-item">
+            <p>Github</p>
+            <a href="https://github.com/vivxz" target="_blank">vivxz</a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Contact;
