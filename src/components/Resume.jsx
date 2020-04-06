e5deb13df5b5859507436b272655e0eb7144b10a
Vivian Quan <viviannquan@gmail.com>
2020-04-05 21:40:52 -0700

    [prog]: Updated resume link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Resume = () => {
   return (
     <div className="resume-container">
-      <object width="100%" height="450px" data="https://vivs-portfolio.s3-us-west-1.amazonaws.com/Resume.pdf" type="application/pdf" />
+      <object width="100%" height="450px" data="https://vivs-portfolio.s3-us-west-1.amazonaws.com/resume.pdf" type="application/pdf" />
     </div>
   )
 }
