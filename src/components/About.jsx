60f4a78e67a4bf836a72f91780752f2fb036f43f
Vivian Quan <viviannquan@gmail.com>
2020-04-05 21:33:18 -0700

    [prog]: Added tech stack to about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,6 +21,18 @@ const About = () => {
           encourage my passions, and continue to
           challenge me as I grow in this field or in life.
       </p>
+
+        <hr />
+        <h3>Technical Skills</h3>
+        <div className="techskills">
+          <p>Front End: Javascript (ES5 & ES6) | HTML5 | CSS3 | React | JQuery | React Native</p>
+
+          <p>Back End: Node.js | Express | PostgreSQL | MongoDB | Mongoose | MySQL | RESTful API | NGINX | Firebase</p>
+
+          <p>Testing/Deployment: Mocha | Chai | AWS EC2 | AWS S3 | Heroku | New Relic | Artillery | Agile Methodology</p>
+
+          <p>Developer Tools: Git | npm | yarn | Webpack | Babel | TDD </p>
+        </div>
       </div>
     </div>
   )
