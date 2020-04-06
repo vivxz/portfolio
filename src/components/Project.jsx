87243eea273d685f126676dc7d9367764d0f0a6e
Vivian Quan <viviannquan@gmail.com>
2020-04-05 21:29:28 -0700

    [prog]: Added skeleton for additional apps

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -21,6 +21,36 @@ const Project = () => {
           </div>
         </div>
       </div>
+      <div classname="apps-sections fe">
+        <h2>Anthropologie Clone - Back End</h2>
+        <div className="techstack">
+          <p>PostgreSQL | Express | Node.js | New Relic | AWS EC2 | Artillery.io | loader.io | Nginx</p>
+          <div className="appreview">
+            PREVIEW HERE!!!
+          </div>
+          <div className="app-description">
+            <p>EXPLAIN APPLICATION HERE</p>
+            <li>Created and utilized a script to generate <strong>10M</strong> mock records to populate both SQL and noSQL databases to emulate a realistic testing scenario </li>
+            <li>Compared both DBMS by stress testing and reduced query times by using <strong>single-field index</strong> resulting in a shortened average latency of  <strong>~4ms</strong> from <strong>~17ms</strong></li>
+            <li>Deployed and horizontally scaled 3 instances to handle <strong>2,750 RPS</strong> with <strong>0.0% error rate</strong> and <strong>67ms average response time</strong> using <strong>round robin</strong> load balancing algorithm</li>
+          </div>
+        </div>
+      </div>
+      <div classname="apps-sections fe">
+        <h2>PAVÉ - Full-Stack</h2>
+        <div className="techstack">
+          <p>MongoDB | Mongoose | Node.js | Express | React | AWS EC2</p>
+          <div className="appreview">
+            PREVIEW HERE!!!
+          </div>
+          <div className="app-description">
+            <p>EXPLAIN APPLICATION HERE</p>
+            <li>Incorporated a <strong>monolithic design architecture</strong> and <strong>agile methodology</strong> to develop and deploy a responsive mobile-first web application that combined aspects of food, drinks, and social media</li>
+            <li>Integrated <strong>Facebook authentication API</strong> to provide users an efficient sign-up and/or login interface</li>
+            <li>Utilized <strong>Yelp</strong> and <strong>Google Maps API</strong> to integrate restaurant information based on location and category</li>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
