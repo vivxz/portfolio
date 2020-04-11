01ba002dcadb9d1a59fe1a3672ac556266ff0f3f
Vivian Quan <viviannquan@gmail.com>
2020-04-10 23:39:09 -0700

    [prog]: Created file containing all tech stacks

---
+++ src/data/stacks.js
@@ -0,0 +1,97 @@
+const stacks = {
+  'frontend': [
+    {
+      'name': 'Javascript (ES5 & ES6)',
+      'bw': 'devicon-javascript-plain',
+      'color': 'devicon-javascript-plain colored'
+    },
+    {
+      'name': 'HTML5',
+      'bw': 'devicon-html5-plain',
+      'color': 'devicon-html5-plain-wordmark colored'
+    },
+    {
+      'name': 'CSS3',
+      'bw': 'devicon-css3-plain',
+      'color': 'devicon-css3-plain-wordmark colored'
+    },
+    {
+      'name': 'React',
+      'bw': 'devicon-react-original',
+      'color': 'devicon-react-original-wordmark colored'
+    },
+    {
+      'name': 'JQuery',
+      'bw': 'devicon-jquery-plain',
+      'color': 'devicon-jquery-plain-wordmark colored'
+    },
+  ],
+  'backend': [
+    {
+      'name': 'Node.js',
+      'bw': 'devicon-nodejs-plain',
+      'color': 'devicon-nodejs-plain-wordmark colored'
+    },
+    {
+      'name': 'Express',
+      'bw': 'devicon-express-original colored',
+      'color': 'devicon-express-original-wordmark colored'
+    },
+    {
+      'name': 'PostgreSQL',
+      'bw': 'devicon-postgresql-plain',
+      'color': 'devicon-postgresql-plain-wordmark colored'
+    },
+    {
+      'name': 'MongoDB',
+      'bw': 'devicon-mongodb-plain',
+      'color': 'devicon-mongodb-plain-wordmark colored'
+    },
+    {
+      'name': 'MySQL',
+      'bw': 'devicon-mysql-plain',
+      'color': 'devicon-mysql-plain-wordmark colored'
+    },
+    {
+      'name': 'NGINX',
+      'bw': 'devicon-nginx-plain',
+      'color': 'devicon-nginx-plain colored'
+    }
+  ],
+  'testing': [
+    {
+      'name': 'Mocha',
+      'bw': 'devicon-mocha-plain',
+      'color': 'devicon-mocha-plain colored'
+    },
+    {
+      'name': 'AWS (EC2 & S3)',
+      'bw': 'devicon-amazonwebservices-original',
+      'color': 'devicon-amazonwebservices-plain-wordmark colored'
+    },
+    {
+      'name': 'Heroku',
+      'bw': 'devicon-heroku-original',
+      'color': 'devicon-heroku-original-wordmark colored'
+    }
+  ],
+  'tools': [
+    {
+      'name': 'Git',
+      'bw': 'devicon-git-plain',
+      'color': 'devicon-git-plain-wordmark colored'
+    },
+    {
+      'name': 'Webpack',
+      'bw': 'devicon-webpack-plain',
+      'color': 'devicon-webpack-plain-wordmark colored'
+    },
+    {
+      'name': 'Babel',
+      'bw': 'devicon-babel-plain',
+      'color': 'devicon-babel-plain colored'
+    }
+  ]
+}
+
+export default stacks;
