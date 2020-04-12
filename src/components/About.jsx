e20a7f84f3edae4964795822131fc0ecd474ba3c
Vivian Quan <viviannquan@gmail.com>
2020-04-11 20:17:51 -0700

    [prog]: Updated styling in both About and Contact component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,59 +12,62 @@ const About = ({ showDialog, navAbout, close }) => {
             CLOSE
             </button>
           <div className="about-info">
-            <img className="profile-picture" src="https://vivs-portfolio.s3-us-west-1.amazonaws.com/pp.png" alt="avatar" />
-            <div className="about-intro">
-              <p>
-                A meticulous, creative, and passionate
-                software engineer with a strong interest in
-                projects that require both conceptual and
-                analytical thinking. Fully-committed to
-                designing and developing innovative
-                applications that users will love. Always
-                eager to learn more from anyone or
-                anything.
+            <h3 className="about-header">About Me</h3>
+            <div className="all-me">
+              <img className="profile-picture" src="https://vivs-portfolio.s3-us-west-1.amazonaws.com/pp.png" alt="avatar" />
+              <div className="about-intro">
+                <p>
+                  A meticulous, creative, and passionate
+                  software engineer with a strong interest in
+                  projects that require both conceptual and
+                  analytical thinking. Fully-committed to
+                  designing and developing innovative
+                  applications that users will love. Always
+                  eager to learn more from anyone or
+                  anything.
               </p>
-              <p>
-                I would love to join a team/company that
-                will allow me to leverage my skills,
-                encourage my passions, and continue to
-                challenge me as I grow in this field or in life.
+                <p>
+                  I would love to join a team/company that
+                  will allow me to leverage my skills,
+                  encourage my passions, and continue to
+                  challenge me as I grow in this field or in life.
               </p>
-              <hr />
-              <h3>Technical Skills</h3>
-              <div className="techskills">
-                <div className="stack-name">Front End:</div>
-                <div className="stack-type">
-                  {stacks.frontend.map((stack, i) => {
-                    return (
-                      <Skills stack={stack} key={i} index={i} />
-                    )
-                  })}
-                </div>
-                <div className="stack-name">Back End:</div>
-                <div className="stack-type">
-                  {stacks.backend.map((stack, i) => {
-                    return (
-                      <Skills stack={stack} key={i} index={i} />
-                    )
-                  })}
-                </div>
-                <div className="stack-name">Testing/Deployment:</div>
-                <div className="stack-type">
-                  {stacks.testing.map((stack, i) => {
-                    return (
-                      <Skills stack={stack} key={i} index={i} />
-                    )
-                  })}
-                </div>
-                <div className="stack-name">Developer Tools:</div>
-                <div className="stack-type">
-                  {stacks.tools.map((stack, i) => {
-                    return (
-                      <Skills stack={stack} key={i} index={i} />
-                    )
-                  })}
-                </div>
+              </div>
+            </div>
+            <hr />
+            <h3 className="about-header">Technical Skills</h3>
+            <div className="techskills">
+              <div className="stack-name">Front End:</div>
+              <div className="stack-type">
+                {stacks.frontend.map((stack, i) => {
+                  return (
+                    <Skills stack={stack} key={i} index={i} />
+                  )
+                })}
+              </div>
+              <div className="stack-name">Back End:</div>
+              <div className="stack-type">
+                {stacks.backend.map((stack, i) => {
+                  return (
+                    <Skills stack={stack} key={i} index={i} />
+                  )
+                })}
+              </div>
+              <div className="stack-name">Testing/Deployment:</div>
+              <div className="stack-type">
+                {stacks.testing.map((stack, i) => {
+                  return (
+                    <Skills stack={stack} key={i} index={i} />
+                  )
+                })}
+              </div>
+              <div className="stack-name">Developer Tools:</div>
+              <div className="stack-type">
+                {stacks.tools.map((stack, i) => {
+                  return (
+                    <Skills stack={stack} key={i} index={i} />
+                  )
+                })}
               </div>
             </div>
           </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,59 +1,63 @@
 import React from 'react';
 import { Dialog } from "@reach/dialog";
+import { MdMailOutline } from "react-icons/md";
+import { FaLinkedinIn } from "react-icons/fa";
+import { GoMarkGithub } from "react-icons/go";
 
 const Contact = ({ navContact, showDialog, close }) => {
   return (
     <>
       {navContact ? (
-        <Dialog isOpen={showDialog} onDismiss={close} aria-labelledby="about-info">
+        <Dialog isOpen={showDialog} onDismiss={close} aria-labelledby="contact" className="contacts">
           <button className="close-button" onClick={close}>
             CLOSE
           </button>
-          <div className="contact-container" id="contact">
-            <div className="contact-card">
+          <div className="contact-container">
+            <div className="contact-info">
               <div className="contact-form">
                 <form className="form" action="https://formspree.io/mlewdvkk" method="POST">
-                  <h1>Contact me</h1>
+                  <div className="form-header">Contact me</div>
                   <div className="form-item">
-                    <label>
+                    <label className="form-labels">
                       Name:
-                <input type="text" name="name" className="name" required />
                     </label>
+                    <input type="text" name="name" id="name" required />
                   </div>
                   <div className="form-item">
-                    <label>
+                    <label className="form-labels">
                       Email:
-              <input type="text" name="_replyto" id="email" required />
                     </label>
+                    <input type="text" name="_replyto" id="email" required />
                   </div>
                   <div className="form-item">
-                    <label>
+                    <label className="form-labels">
                       Subject:
-              <input type="text" name="subject" id="subject" required />
                     </label>
+                    <input type="text" name="subject" id="subject" required />
                   </div>
                   <div className="form-item">
-                    <label>
+                    <label className="form-labels">
                       Message:
-              <textarea name="message" id="message" required />
                     </label>
+                    <textarea name="message" id="message" required />
                   </div>
-                  <input type="submit" value="SEND" />
+                  <input type="submit" className="send-btn" value="SEND" />
                 </form>
               </div>
-              <div className="contact-info">
-                <h1>Let's get in touch</h1>
-                <div className="form-item">
-                  <p>Email</p>
-                  <p>viviannquan@gmail.com</p>
+              <div className="split"></div>
+              <div className="reach-me">
+                <div className="form-header">Get in touch</div>
+                <div className="reach-item">
+                  <MdMailOutline className="devicons" />
+                  <span className="reach-info">viviannquan@gmail.com </span>
                 </div>
-                <div className="form-item">
-                  <p>LinkedIn</p>
-                  <a href="https://www.linkedin.com/in/vivian-quan/" target="_blank">vivian-quan</a>
+                <div className="reach-item">
+                  <FaLinkedinIn className="devicons" />
+                  <a href="https://www.linkedin.com/in/vivian-quan/" className="reach-info" target="_blank">vivian-quan</a>
                 </div>
-                <div className="form-item">
-                  <p>Github</p>
-                  <a href="https://github.com/vivxz" target="_blank">vivxz</a>
+                <div className="reach-item">
+                  <GoMarkGithub className="devicons" />
+                  <a href="https://github.com/vivxz" className="reach-info" target="_blank">vivxz</a>
                 </div>
               </div>
             </div>
